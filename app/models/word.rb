9b423ba3c6e5481ec6447aaab3ad824a255e1eed
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-16 18:37:18 -0500

    it works

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,20 +1,18 @@
 class Word < ActiveRecord::Base
   # Remember to create a migration!
   def self.anagrams(user_word)
-    # return the list of words
-    counter = 1
+
     user_sorted_word_array = user_word.downcase.split('').sort
+    puts user_sorted_word_array
+
     anagrams_matches = []
-
-    while counter <= Word.count
-      current_word = Word.find(counter).entry
-      current_word_array = current_word.downcase.split('').sort
+    Word.all.each do |current_word|
+      current_word_array = current_word.entry.downcase.split('').sort
       if current_word_array == user_sorted_word_array
-        anagrams_matches << current_word
+        puts current_word.entry + " " + current_word_array.inspect
+        anagrams_matches << current_word.entry
       end
-      counter += 1
     end
-
     anagrams_matches
   end
 
